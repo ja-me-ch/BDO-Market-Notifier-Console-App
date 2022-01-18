a6dd9ecdf131303a4ecc78f482ea6374834281c5
ja-me-ch <60724072+ja-me-ch@users.noreply.github.com>
2022-01-17 20:17:53 -0500

    Adding AWS SNS SMS support

--- BDO Market Notifier Console App/index.js
+++ BDO Market Notifier Console App/index.js
@@ -1,6 +1,13 @@
 const dotenv = require('dotenv')
 dotenv.config()
 
+const aws = require('aws-sdk');
+aws.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION
+});
+
 const axios = require('axios');
 const config = {
     method: 'post',
@@ -8,10 +15,20 @@ const config = {
     headers: {}
 };
 
-const blackstarMainhandIds = [715001, 715003, 715005, 715007, 715009, 715011, 715013, 715016, 715017, 715019, 715021, 718616, 690563, 692045, 730564, 732313, 733063, 11630]
+const blackstarMainhandIds = [715001, 715003, 715005, 715007, 715009, 715011, 715013, 715016, 715017, 715019, 715021, 718616, 690563, 692045, 730564, 732313, 733063, 12257]
 let liveAtTimes = [];
 
 console.log('BDO Market Notifier Console App');
+
+const sendSMS = (publishParams) => {
+    const sns = new aws.SNS({ apiVersion: '2010-03-31' }).publish(publishParams).promise()
+        .then((response) => {
+            console.log(response);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+}
 
 const checkArrayForItem = function (items) {
     for (let i = 0; i < items.length; i++) {
@@ -21,6 +38,14 @@ const checkArrayForItem = function (items) {
             }
             else {
                 liveAtTimes.push(items[i].liveAt);
+
+                let publishParams = {
+                    Message: `${items[i].name} FOUND!`,
+                    PhoneNumber: process.env.RECEIVING_PHONE_NUMBER
+                };
+
+                sendSMS(publishParams);
+
                 console.log(`${items[i].name} FOUND!`);
                 //Insert Callback to send text message to my phone
             }
@@ -39,4 +64,4 @@ setInterval(async () => {
     if (items.length >= 1) {
         checkArrayForItem(items);
     }
-}, 60000)
+}, 5000)
